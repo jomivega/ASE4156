db7904a3b3f3aee8a8e0f03897bc3a4b2ad96c0c
Nigel Schuster <Neitsch@users.noreply.github.com>
2017-09-18 17:39:35 -0400

    Satisfy linter (#9)

--- trading/apps.py
+++ trading/apps.py
@@ -1,5 +1,11 @@
+"""
+Trading represents all the user interaction to perform trades
+"""
 from django.apps import AppConfig
 
 
 class TradingConfig(AppConfig):
+    """
+    Standard config
+    """
     name = 'trading'
