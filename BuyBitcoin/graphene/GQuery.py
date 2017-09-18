fd1d491c4bd5d688fa8714fb89a5a52fa86add9c
Nigel Schuster <nigel.schusters@googlemail.com>
2017-09-18 00:36:31 -0400

    Easy pagination

--- BuyBitcoin/graphene/GQuery.py
+++
@@ -1,9 +0,0 @@
-import graphene
-from .GUser import GUser
-
-
-class GQuery(graphene.ObjectType):
-    viewer = graphene.Field(GUser, )
-
-    def resolve_viewer(self, args, context, info):
-        return context.user
