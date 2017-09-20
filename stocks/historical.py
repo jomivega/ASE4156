f0f923769e23faf1d5f4a503ab435c823e4343a2
jordan vega <jomivega400@gmail.com>
2017-09-20 14:36:58 -0400

    Fixing lint

--- stocks/historical.py
+++ stocks/historical.py
@@ -16,14 +16,13 @@ def data_ten_years_back_for_stock(request):
         ticker = body['ticker']
         stock = Stock(name=name, ticker=ticker)
         stock.save()
-        stock_id = stock.id
         now = arrow.now()
         ten_back = now.replace(years=-10)
         now = get_date_array_for_fetcher(now)
         ten_back = get_date_array_for_fetcher(ten_back)
         fetcher = Fetcher(ticker, ten_back, now)
         history = fetcher.getHistorical()
-        save_stock_quote_from_fetcher(history, stock_id)
+        save_stock_quote_from_fetcher(history, stock.id)
 
 
 def get_date_array_for_fetcher(arrow_date):
@@ -34,14 +33,17 @@ def get_date_array_for_fetcher(arrow_date):
 def fill_stocks(request):
     """Function that fills stock data for missing days"""
     if request.method == "POST":
-        data = DailyStockQuote.objects.values('stock__ticker', 'stock_id')\
-                .annotate(date=Max('date'))
+        stock_id_field = 'stock_id'
+        stock_ticker = 'stock__ticker'
+        date = 'date'
+        data = DailyStockQuote.objects.values(stock_ticker, stock_id_field)\
+                .annotate(date=Max(date))
         for stock in data:
-            last_date = arrow.get(stock["date"]).replace(days=+1)
+            last_date = arrow.get(stock[date]).replace(days=+1)
             last_date = get_date_array_for_fetcher(last_date)
             now = get_date_array_for_fetcher(arrow.now())
-            ticker = stock["stock__ticker"]
-            stock_id = stock['stock_id']
+            ticker = stock[stock_ticker]
+            stock_id = stock[stock_id_field]
             fetcher = Fetcher(ticker, last_date, now)
             history = fetcher.getHistorical()
             save_stock_quote_from_fetcher(history, stock_id)
