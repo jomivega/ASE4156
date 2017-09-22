b3761d470b2acd0af149066f214577df1e0ec9bf
jordan vega <jomivega400@gmail.com>
2017-09-22 13:25:19 -0400

    Adding App Bar and Cards

--- stories/index.jsx
+++ stories/index.jsx
@@ -4,6 +4,7 @@ import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+<<<<<<< HEAD
 import { number, text } from '@storybook/addon-knobs';
 
 import AppBar from '../web/js/components/AppBar';
@@ -18,3 +19,27 @@ storiesOf('Page', module).add('Test page', () => {
   const res = <PageTest />;
   return res;
 });
+=======
+import { muiTheme } from 'storybook-addon-material-ui';
+import appTheme from '../web/js/theme/muiTheme'
+import { Button, Welcome } from '@storybook/react/demo';
+
+import Shares from '../web/js/components/shares';
+import AppBar from '../web/js/components/appbar';
+import {Card_WF, Media_Card_WF} from '../web/js/components/card_wf';
+
+
+storiesOf('Shares', module)
+  .addDecorator(muiTheme(appTheme))
+  .add('simple', () => <Shares />);
+
+
+storiesOf('AppBar', module)
+.addDecorator(muiTheme(appTheme))
+.add('Main app bar', () => <AppBar/>);
+
+storiesOf('Cards', module)
+  .addDecorator(muiTheme(appTheme))
+  .add('Simple card', () => <Card_WF title="Title" subtitle="sub" cardText="fdsa"/>)
+  .add('Content card', () => <Media_Card_WF content = {<h1>Content</h1>}/>);
+>>>>>>> Adding App Bar and Cards
