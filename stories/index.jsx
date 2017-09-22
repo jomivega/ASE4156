df16f2619e7df426908e6dc230033f7bd4b57645
jordan vega <jomivega400@gmail.com>
2017-09-22 13:25:19 -0400

    Adding App Bar and Cards

--- stories/index.jsx
+++ stories/index.jsx
@@ -5,17 +5,24 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { muiTheme } from 'storybook-addon-material-ui';
-
+import appTheme from '../web/js/theme/muiTheme'
 import { Button, Welcome } from '@storybook/react/demo';
 
 import Shares from '../web/js/components/shares';
+import AppBar from '../web/js/components/appbar';
+import {Card_WF, Media_Card_WF} from '../web/js/components/card_wf';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
 storiesOf('Shares', module)
-  .addDecorator(muiTheme())
+  .addDecorator(muiTheme(appTheme))
   .add('simple', () => <Shares />);
+
+
+storiesOf('AppBar', module)
+.addDecorator(muiTheme(appTheme))
+.add('Main app bar', () => <AppBar/>);
+
+storiesOf('Cards', module)
+  .addDecorator(muiTheme(appTheme))
+  .add('Simple card', () => <Card_WF title="Title" subtitle="sub" cardText="fdsa"/>)
+  .add('Content card', () => <Media_Card_WF content = {<h1>Content</h1>}/>);
