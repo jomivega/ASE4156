7772fff89108f720d0829d69c17ac7663aedd2cd
Nigel Schuster <Neitsch@users.noreply.github.com>
2017-09-29 09:57:44 -0400

    Fix this annoying prop types warning (#41)

--- web/js/components/HighlightBox/HighlightBox.jsx
+++ web/js/components/HighlightBox/HighlightBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import Card, {CardHeader, CardMedia, CardContent, CardActions} from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import List, {ListItem, ListItemText,} from 'material-ui/List';
@@ -8,7 +8,7 @@ class HighlightBox extends React.Component {
   static propTypes = {
     title: PropTypes.any.isRequired,
     value: PropTypes.any.isRequired,
-    secondaryInfo: PropTypes.arrayOf(PropTypes.shape({value: PropTypes.any.isRequired, text: PropTypes.any.isRequired,})),
+    secondaryInfo: PropTypes.arrayOf(PropTypes.shape({value: PropTypes.any.isRequired, text: PropTypes.any.isRequired}))
   }
   render() {
     console.log("Render");
@@ -22,7 +22,7 @@ class HighlightBox extends React.Component {
             {this.props.secondaryInfo
               ? this.props.secondaryInfo.map(secondary => {
                 return (
-                  <ListItem>
+                  <ListItem key={secondary.text}>
                     <ListItemText>{secondary.value + " " + secondary.text}</ListItemText>
                   </ListItem>
                 );
