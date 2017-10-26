2e8bde97c8740afa04ad14bfb29381c7248c0459
Nigel Schuster <Neitsch@users.noreply.github.com>
2017-10-26 11:54:11 -0400

    Fix mutation problem (#94)

--- web/js/components/InvestBucket/InvestBucketRelay.jsx
+++ web/js/components/InvestBucket/InvestBucketRelay.jsx
@@ -77,7 +77,7 @@ class InvestBucketRelay extends React.Component<Props, State> {
       this.props.relay.environment,
     )(
       {
-        config: chunks.map(c => ({ id: c.id, quantity: c.quantity })),
+        config: chunks.map(c => ({ idValue: c.id, quantity: c.quantity })),
         id: this.props.bucket.id,
       },
     );
